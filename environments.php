cb5b20dfe43643b19dfa612d4ad37517f07c44bf
Tim Langley <tim.langley@canddi.com>
2011-01-27 20:53:00 +0000

    first commit

---
+++ environments.php
@@ -0,0 +1,14 @@
+<?
+/**
+ *	@purpose			
+ *
+ *	@author 	Tim Langley
+ *	@date		2010-09-01
+ *
+**/
+
+defined('ENVIRONMENT_DEVELOPMENT') 	or define('ENVIRONMENT_DEVELOPMENT','development');
+defined('ENVIRONMENT_TEST') 		or define('ENVIRONMENT_TEST',		'test');
+defined('ENVIRONMENT_UNIT_TEST') 	or define('ENVIRONMENT_UNIT_TEST',	'unit-test');
+defined('ENVIRONMENT_UAT') 			or define('ENVIRONMENT_UAT',		'UAT');
+defined('ENVIRONMENT_PRODUCTION') 	or define('ENVIRONMENT_PRODUCTION',	'production');
