3f15ca97601fb2eea7cae99ab73ba4879bac3eb9
Franco Zeoli <zeolifranco@gmail.com>
2011-01-30 21:30:19 -0300

    Added license and removed useless file.

--- src/main/php/Qwerly/API/Response.php
+++ src/main/php/Qwerly/API/Response.php
@@ -1,5 +1,22 @@
 <?php
+/**
+ * Qwerly API
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://github.com/canddi/Zend_Qwerly/blob/master/LICENSE.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to hello@canddi.com so we can send you a copy immediately.
+ *
+ */
 
+/**
+ * Qwerly's response object.
+ */
 class Qwerly_API_Response
 {
 
