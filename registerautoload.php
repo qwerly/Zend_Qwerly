4aedbf68d47ce5047e3d5e0928c0d446de2e8726
Franco Zeoli <zeolifranco@gmail.com>
2011-01-31 00:42:40 -0300

    Tabs and useless stuf.

--- registerautoload.php
+++ registerautoload.php
@@ -1,21 +1,16 @@
-<?
+<?php
 /**
- *	This is used in the Testing and Validation phase of the mvn test
-**/	
+ *    This is used in the Testing and Validation phase of the mvn test
+**/
 
-define('QWERLY_PATH', 		realpath(dirname(__FILE__)).'/src/main/php/Qwerly');
-define('QWERLY_TEST_PATH',	realpath(dirname(__FILE__)).'/src/test/php/Qwerly/');
-$paths = array(	get_include_path(),
-				QWERLY_PATH,
-				realpath(dirname(__FILE__)).'/src/main/php',
-				realpath(dirname(__FILE__)).'/target/phpinc',
-				'.');
+define('QWERLY_PATH',         realpath(dirname(__FILE__)).'/src/main/php/Qwerly');
+define('QWERLY_TEST_PATH',    realpath(dirname(__FILE__)).'/src/test/php/Qwerly/');
+$paths = array(    get_include_path(),
+                QWERLY_PATH,
+                realpath(dirname(__FILE__)).'/src/main/php',
+                realpath(dirname(__FILE__)).'/target/phpinc',
+                '.');
 set_include_path(implode(PATH_SEPARATOR, $paths));
-
-require_once 'environments.php';
-defined('APPLICATION_ENV')		or define('APPLICATION_ENV', 	ENVIRONMENT_UNIT_TEST);
-defined('APPLICATION_PATH') 	or define('APPLICATION_PATH', 	realpath(dirname(__FILE__)).'/src/main/php/Qwerly');
-defined('MOCK_DB_PATH')			or define('MOCK_DB_PATH', 		realpath(dirname(__FILE__)).'/src/test/testDB/');
 
 require_once "Zend/Loader/Autoloader.php";
 $autoloader = Zend_Loader_Autoloader::getInstance();
